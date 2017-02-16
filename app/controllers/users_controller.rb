db1ba631ace4bc0478254d78da27900bbcbfd3a5
Rakesh Verma <verma.rakesh@oyorooms.com>
2017-02-16 17:50:34 +0530

    Trying to make the login link work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path, :notice => "Signed up!"
+      redirect_to user_path(@user), :notice => "Signed up!"
     else
       render "new"
     end
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
   end
 
   def user_params
