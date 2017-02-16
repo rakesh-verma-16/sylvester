834ec3238a0a1024712e68bfa883d9cd7bdd11cb
Rakesh Verma <verma.rakesh@oyorooms.com>
2017-02-16 11:31:06 +0530

    added DOB and description name in login form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,13 +6,13 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to new_user_path, :notice => "Signed up!"
+      redirect_to user_path, :notice => "Signed up!"
     else
       render "new"
     end
   end
 
   def user_params
-    params.require(:user).permit(:name, :email, :dob, :password)
+    params.require(:user).permit(:name, :email, :date_of_birth, :password, :description)
   end
 end
